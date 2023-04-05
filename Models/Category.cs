c8acbdd6dfe02960b7c0404db0f9182cabb449be
Moises Alves <moisesalves1@gmail.com>
2023-04-05 01:11:15 -0300

    Repositório Genérico!

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,7 +6,7 @@ namespace Blog.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Slug { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace Blog
             connection.Open();
             ReadUsers(connection);
             ReadRoles(connection);
+            ReadTags(connection);
             // ReadUser();
             // CreateUser();
             // UpdateUser();
@@ -25,19 +26,28 @@ namespace Blog
         public static void ReadUsers(SqlConnection connection)
         {
             var repository = new Repository<User>(connection);
-            var users = repository.Get();
+            var items = repository.Get();
             
-            foreach (var user in users)
-                Console.WriteLine(user.Name);
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
 
         public static void ReadRoles(SqlConnection connection)
         {
-            var repository = new RoleRepository(connection);
-            var roles = repository.Get();
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
             
-            foreach (var role in roles)
-                Console.WriteLine(role.Name);
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+            
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
 
     }
